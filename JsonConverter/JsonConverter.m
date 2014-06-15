3846c6433bb2b1f9c628016e93af44d6b7c8043a
Jonathan Almeida <jonathan.almeida942@gmail.com>
2014-06-15 17:32:42 -0400

    Initial Commit

---
+++ JsonConverter/JsonConverter.m
@@ -0,0 +1,13 @@
+//
+//  JsonConverter.m
+//  JsonConverter
+//
+//  Created by Jonathan Almeida on 2014-06-15.
+//  Copyright (c) 2014 Jonathan Almeida. All rights reserved.
+//
+
+#import "JsonConverter.h"
+
+@implementation JsonConverter
+
+@end
---
+++ JsonConverterTests/JsonConverterTests.m
@@ -0,0 +1,34 @@
+//
+//  JsonConverterTests.m
+//  JsonConverterTests
+//
+//  Created by Jonathan Almeida on 2014-06-15.
+//  Copyright (c) 2014 Jonathan Almeida. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JsonConverterTests : XCTestCase
+
+@end
+
+@implementation JsonConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
